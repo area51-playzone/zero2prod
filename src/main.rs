400945bd0dcb8691d88625281e6230ffa885cf36
Syed Ishtiaque Ahmad <syed@dr-bill.ca>
2022-12-17 01:18:11 -0800

    Pass TcpListen to bind to address and port dynamically - 127.0.0.1:0 -> OS tries to find free port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
@@ -6,5 +7,6 @@ async fn main() -> std::io::Result<()> {
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
+    run(listener)?.await
 }
